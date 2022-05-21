e06db0f0c82dca9c64e416a8a6189b2cda7d5db1
Tonmoy175 <41666183+Tonmoy175@users.noreply.github.com>
2022-05-21 19:04:27 +0600

    Add files via upload

---
+++ Free1.py
@@ -0,0 +1,21 @@
+import os, platform,time
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('clear')
+    print(' \033[1;32mFacebook Is On Update All Method Are Going Lol');time.sleep(1)
+    print(' \033[1;32mI will Try to Find New Method Inshaallah');time.sleep(1)
+    from irfan import ___RecodeSampah__
+    ___RecodeSampah__()
+    exit()
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    print('Sorry But Your Mobile Dosent Support  This Tool')
